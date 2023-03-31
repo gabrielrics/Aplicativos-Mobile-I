dd6f080d8d433249fb0346885863b09ea88b00b9
gabrielrics <gabriel908henrique@gmail.com>
2023-03-30 22:23:42 -0300

    Agenda de duas telas

--- Agenda/app/src/main/java/com/example/agenda/MainActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btInicio = findViewById(R.id.Cadastro);                 //R é uma classe.
+        btInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                abreSegundaTela();
+
+            }
+        });
+    }
+
+    private void abreSegundaTela(){
+
+
+        Intent it = new Intent(  this, Cadastro.class);
+        startActivityForResult(it, 1);
+
     }
 }
